4fce910f5a00f07963b21a5f9fe4d0f25cebf563
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-08-09 13:58:06 +0900

    v 1.0 Initialize server and auth handlers

---
+++ server/internal/router/authRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controller/authController');
+
+// ${URL}/api/auth/login-user
+router.post('/login-user', controller.login);
+
+// ${URL}/api/auth/register-user
+router.post('/register-user', controller.register);
+
+module.exports = router;
