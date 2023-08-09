e908b3a0d84513dc849275c60d1508b41f0d1715
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-08-10 03:23:07 +0900

    v 1.3 Login controller, middleware requires

--- server/internal/controller/authController.js
+++ server/internal/controller/authController.js
@@ -1,11 +1,36 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../model/userModel');
 
-module.exports.login = function (req, res) {
-    res.status(200).json({
-        login: 'from controller',
-    })
+module.exports.login = async function (req, res) {
+    const candidate = await User.findOne({where: { email: req.body.email }});
+
+    if (candidate === null) {
+        res.status(404).json({
+            message: 'User not found',
+        });
+    } else {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if (!passwordResult) {
+            res.status(401).json({
+                message: "Passwords doesn't match",
+            });
+        } else {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate.id,
+            },
+                process.env.JWT,
+                {
+                    expiresIn: 60 * 60 * 24 * 7,
+                }
+            );
+            res.status(200).json({
+                token: `Bearer ${token}`,
+            });
+        }
+    }
 }
 
 module.exports.register = async function (req, res) {
